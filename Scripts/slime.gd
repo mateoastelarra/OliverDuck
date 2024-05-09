0e1bdb61d55da084018ad3048ca7c4e600c7dfd6
Mateo Astelarra <mateo@astelarra.com>
2024-05-09 16:43:03 -0300

    Slime sprite slip and player improved dying

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -3,14 +3,17 @@ extends Node2D
 const SPEED = 60
 
 var direction = 1
-@onready var ray_cast_2d_right = $RayCast2DRight
-@onready var ray_cast_2d_left = $RayCast2DLeft
+@onready var ray_cast_right = $RayCast2DRight
+@onready var ray_cast_left = $RayCast2DLeft
+@onready var animated_sprite = $AnimatedSprite2D
 
 
 func _process(delta):
-	if ray_cast_2d_right.is_colliding():
+	if ray_cast_right.is_colliding():
 		direction = - 1
-	if ray_cast_2d_left.is_colliding():
+		animated_sprite.flip_h = true
+	if ray_cast_left.is_colliding():
 		direction = 1
+		animated_sprite.flip_h = false
 		
 	position.x += direction * SPEED * delta
