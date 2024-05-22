0732041fb19f0fbb891e36b9f7c41a969301eda7
Julian Zaltron <zaltron.julian@gmail.com>
2024-05-21 21:27:37 -0300

    Making a better shader. Reemplacing my global time var for godots4 TIME

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,16 +1,3 @@
 extends Node
 
 var player
-
-#var fix_time  = OS.get_ticks_msec() - floor(OS.get_ticks_msec()/3600)*3600
-var time = 0.0;
-
-#func _ready():
-
-
-func _process(delta):
-	#fondo.gdshader.set_uniform_value("time", time)
-	time += delta
-	RenderingServer.global_shader_parameter_set("time", time);
-	print("time is: ")
-	print(time)
