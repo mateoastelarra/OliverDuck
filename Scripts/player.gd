caafc10dccc84f33ad563afaf4c8f242faa9754d
Mateo Astelarra <mateo@astelarra.com>
2024-05-23 12:41:36 -0300

    Limit falling velocity

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,13 +5,15 @@ signal bullet_shot(bullet_scene, location)
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var gravity: int = 980
+@export var falling_velocity_limit: float = 400
+
 var bullet_scene = preload("res://Scenes/bullet.tscn")
 var looking_direction
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var muzzle = $Muzzle
+
 
 
 func _init():
@@ -25,7 +27,7 @@ func _process(delta):
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		handle_gravity(delta)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -83,3 +85,8 @@ func update_shooting_direction():
 	else:
 		looking_direction = Vector2(1,0)
 		
+
+func handle_gravity(delta):
+	velocity.y += gravity * delta
+	velocity.y = minf(velocity.y, falling_velocity_limit)
+	print(velocity.y)
