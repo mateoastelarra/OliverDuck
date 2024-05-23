35c24e351c3dc3bdb3dd5bf831fc0b077a69e9d1
Mateo Astelarra <mateo@astelarra.com>
2024-05-23 17:05:34 -0300

    Basic gliding

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,20 +5,31 @@ signal bullet_shot(bullet_scene, location)
 const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
-@export var gravity: int = 980
+@export var default_gravity: float = 980
 @export var falling_velocity_limit: float = 400
+@export var gliding_ratio: float = 0.3
+@export var gliding_time: float = 1
 
 var bullet_scene = preload("res://Scenes/bullet.tscn")
 var looking_direction
+var gravity: float
+var is_gliding: bool = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var muzzle = $Muzzle
-
+@onready var gliding_timer = $GlidingTimer
 
 
 func _init():
 	Globals.set("player", self)
+
+
+func _ready():
+	gliding_timer.wait_time = gliding_time
+	gliding_timer.one_shot = true
+	gravity = default_gravity
 	
+		
 func _process(delta):
 	if Input.is_action_pressed("shoot"):
 		shoot()
@@ -28,16 +39,19 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		handle_gravity(delta)
-
+	
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		
+	
+		
 	# -1 when left, 1 when right and 0 if none
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	move(direction)
-		
+	manage_glide()
 	flip_sprite(direction)
 	update_shooting_direction()
 	play_animations(direction)
@@ -89,4 +103,23 @@ func update_shooting_direction():
 func handle_gravity(delta):
 	velocity.y += gravity * delta
 	velocity.y = minf(velocity.y, falling_velocity_limit)
-	print(velocity.y)
+
+func manage_glide():
+	if Input.is_action_just_pressed("glide") and !is_on_floor():
+		glide()
+	
+	if Input.is_action_just_released("glide") or is_on_floor():
+		stop_gliding()
+	
+func glide():
+	if !is_gliding:
+		gravity = gravity * gliding_ratio
+		is_gliding = true
+		gliding_timer.start()
+
+func stop_gliding():
+	gravity = default_gravity
+	is_gliding = false
+	
+func _on_gliding_timer_timeout():
+	stop_gliding()
