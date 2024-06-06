434cd2f10f7d88a84db234a586b9fb27eb4fb561
Mateo Astelarra <mateo@astelarra.com>
2024-06-06 11:53:03 -0300

    Movement in SM

--- Scripts/State Machine/States/Player/player_idle.gd
+++ Scripts/State Machine/States/Player/player_idle.gd
@@ -11,10 +11,13 @@ func Exit():
 	pass
 
 func Update(_delta: float):
+	pass
+	
+func Physics_Update(_delta: float):
 	if !player.is_on_floor():
 		Transitioned.emit(self, "PlayerJump")
 	elif Input.get_axis("move_left", "move_right") != 0:
 		Transitioned.emit(self, "PlayerRunning")
+	else:
+		player.velocity = Vector2(0,0)
 	
-func Physics_Update(_delta: float):
-	pass
--- Scripts/State Machine/States/Player/player_running.gd
+++ Scripts/State Machine/States/Player/player_running.gd
@@ -11,21 +11,13 @@ func Exit():
 	pass
 
 func Update(_delta: float):
+	pass
+	
+	
+func Physics_Update(_delta: float):
 	var direction = Input.get_axis("move_left", "move_right")
 	if !player.is_on_floor():
 		Transitioned.emit(self, "PlayerJump")
 	elif direction == 0:
 		Transitioned.emit(self, "PlayerIdle")
 	
-	
-func Physics_Update(_delta: float):
-	pass
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
