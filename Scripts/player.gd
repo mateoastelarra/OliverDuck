dfa17b8fbb0aa869235e6762b3c45e2cd4cba964
Mateo Astelarra <mateo@astelarra.com>
2024-06-06 16:05:22 -0300

    Player script refactored

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,8 +7,6 @@ const JUMP_VELOCITY = -300.0
 
 @export var gravity: float = 980
 @export var falling_velocity_limit: float = 400
-@export var gliding_ratio: float = 0.1
-@export var gliding_time: float = 1
 
 var bullet_scene = preload("res://Scenes/bullet.tscn")
 var looking_direction
@@ -16,17 +14,9 @@ var has_glided: bool = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var muzzle = $Muzzle
-@onready var gliding_timer = $StateMachine/PlayerGliding/GlidingTimer
-
-
 
 func _init():
 	Globals.set("player", self)
-
-
-func _ready():
-	gliding_timer.wait_time = gliding_time
-	gliding_timer.one_shot = true
 	
 		
 func _process(delta):
@@ -34,11 +24,7 @@ func _process(delta):
 		shoot()
 	
 	
-func _physics_process(delta):
-	# Add the gravity.
-	#if not is_on_floor():
-	#	handle_gravity(delta)
-	
+func _physics_process(delta):	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY	
@@ -47,10 +33,8 @@ func _physics_process(delta):
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	move(direction)
-	#manage_glide()
 	flip_sprite(direction)
 	update_shooting_direction()
-	#play_animations(direction)
 	move_and_slide()
 
 
@@ -59,16 +43,6 @@ func move(direction):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-
-func play_animations(direction):
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("Idle")
-		else:
-			animated_sprite.play("Run")
-	else:
-		animated_sprite.play("Jump")
 		
 
 func flip_sprite(direction):
@@ -94,27 +68,3 @@ func update_shooting_direction():
 		looking_direction = Vector2(-1,0)
 	else:
 		looking_direction = Vector2(1,0)
-		
-
-func handle_gravity(delta):
-	if has_glided and velocity.y > 0:
-		velocity.y = gliding_ratio
-	else:
-		velocity.y += gravity * delta
-		velocity.y = minf(velocity.y, falling_velocity_limit)
-
-func manage_glide():
-	if Input.is_action_just_pressed("glide") and !is_on_floor():
-		glide()
-	
-	if Input.is_action_just_released("glide") or is_on_floor():
-		has_glided = false
-	
-func glide():
-	if !has_glided:
-		has_glided = true
-		gliding_timer.start()
-	
-	
-func _on_gliding_timer_timeout():
-	has_glided = false
