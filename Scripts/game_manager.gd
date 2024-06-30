6ec945ca4a8f14715b6cf7e62b58fa2e7ee4746b
Julian Zaltron <zaltron.julian@gmail.com>
2024-06-30 20:36:18 -0300

    Take screenshots by pressing t

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -4,6 +4,10 @@ var score = 0
 var player = null
 var converted_animals = 0
 
+func _input(_event):
+	if Input.is_action_just_pressed("take_screenshot"):
+		take_screenshot()
+
 func add_point():
 	score += 1
 	print(score)
@@ -11,3 +15,11 @@ func add_point():
 func add_converted_animal():
 	converted_animals += 1
 	print(converted_animals)
+
+func take_screenshot():
+	var timestamp = Time.get_datetime_string_from_system(false, true).replace(":", "-")
+	await RenderingServer.frame_post_draw
+	var sshot = get_viewport().get_texture().get_image()
+	#next line (commented) resizes images
+	#sshot.resize(200, 100, Image.INTERPOLATE_TRILINEAR)
+	sshot.save_png("user://screenshot" + timestamp + ".png")
